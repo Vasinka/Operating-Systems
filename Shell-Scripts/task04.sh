9284b21040334e13b259f0efeb54166d0f98b603
Vasilena Kaludova <41026837+Vasinka@users.noreply.github.com>
2018-08-04 14:47:47 +0300

    Create task04.sh

---
+++ Shell-Scripts/task04.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+if [ $# -eq 0 ] 
+    then echo "Please enter at least 1 argument!"
+exit 0
+fi
+
+read string
+
+for i in $*
+do 
+    count=$(grep "string" $i | wc -l)
+    echo "In file $i you have the string \" string \" $count times"
+done
+
+exit 0
